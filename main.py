1e99ebddbfe328c41ebda7ec3af7d642f4d146f4
icaijy <caijunyi08@outlook.com>
2021-04-10 18:34:35 +0800

    size

--- main.py
+++ main.py
@@ -14,38 +14,38 @@ def bw(threshold, path):
     return Img
 
 
-def textimage(text):
+def textimage(text, size=16):
 
-    im = Image.new("RGB", (len(text)*16, 16), (255, 255, 255))
+    im = Image.new("RGB", (len(text)*size, size), (255, 255, 255))
     dr = ImageDraw.Draw(im)
-    font = ImageFont.truetype("C:\\Windows\\Fonts\\DengXian\Deng.ttf", 14)
-    dr.text((1.25, 1.25), text, font=font, fill="#000000")
+    font = ImageFont.truetype("C:\\Windows\\Fonts\\DengXian\Deng.ttf", size)
+    dr.text((0,0), text, font=font, fill="#000000")
     return im
 
-def con(text, threshold=150, color=None):
-    textimage(text).save("miao.png")
+def con(text, threshold=150, color=None, size=16):
+    textimage(text, size=size).save("miao.png")
     im = np.array(bw(threshold, "miao.png"), dtype="int32").tolist()
-    img = [[] for i in range(256)]
-    white = Image.new("RGB", (16, 16), (255, 255, 255))
+    img = [[] for i in range(size*size)]
+    white = Image.new("RGB", (size, size), (255, 255, 255))
     white.save("white.png")
     white = np.array(bw(6, "white.png"),dtype="int32").tolist()
-    for x in range(16):
+    for x in range(size):
         for y in im[x]:
             if y==0:
-                for z in range(16):
-                    img[x*16+z].extend(im[z])
+                for z in range(size):
+                    img[x*size+z].extend(im[z])
 
             else:
-                for z in range(16):
-                    img[x*16+z].extend(white[z])
+                for z in range(size):
+                    img[x*size+z].extend(white[z])
     n = np.array(img)
     if color==None:
         n[n == 1] = 255
         return Image.fromarray(n).convert("L")
     else:
         n = n.tolist()
-        for x in range(256):
-            for y in range(256):
+        for x in range(size*size):
+            for y in range(size*size):
                 if n[x][y]==0:
                     n[x][y]=color
                 else:
@@ -55,8 +55,5 @@ def con(text, threshold=150, color=None):
         return Image.fromarray(n).convert("RGB")
 
 
-
-con("抄",color=[255,0,0]).save("chu.png")
-con("了").save("wan.png")
-con("作").save("ning.png")
-con("文").save("aa.png")
+con("妙",size=64).save("a.png")
+con("啊",size=64).save("b.png")
